8a0a452f64f47a8b6c452c411c813bb13b91f52c
Hirad Pourtahmasbi <git@hirad.codes>
2021-03-13 23:27:50 -0800

    feat: implement /version endpoint

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/version")]
+fn index() -> &'static str {
+    env!("CARGO_PKG_VERSION")
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
+}
