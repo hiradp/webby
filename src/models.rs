5203b68330cbf1f35df5eb9969d6bb093b853b5f
Hirad Pourtahmasbi <git@hirad.codes>
2021-03-21 09:19:44 -0700

    feat: introduce hierarchical and global configuration

---
+++ src/models.rs
@@ -0,0 +1,19 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Person {
+    pub first_name: String,
+    pub last_name: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Job {
+    pub company: String,
+    pub title: String,
+    pub url: String
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Meta {
+    pub favicons: String,
+}
--- src/routes/api/v1/version.rs
+++ src/routes/api/v1/version.rs
@@ -1,6 +1,11 @@
 use rocket_contrib::json::JsonValue;
 
+use crate::SETTINGS;
+
 #[get("/version")]
 pub fn get() -> JsonValue {
-    json!({ "version": env!("CARGO_PKG_VERSION") })
+    json!({
+    "debug": SETTINGS.debug,
+    "version": env!("CARGO_PKG_VERSION"),
+    })
 }
